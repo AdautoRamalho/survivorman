e709d43af7534723b47a8a1d01580ff7c847978d
Adauto Ramalho <a.ramalho@cp-pro.de>
2022-07-09 10:50:49 -0300

    first level perfectly balanced

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -9,12 +9,13 @@ namespace Scrips
     {
         private PlayerInput _playerInput;
         private InputAction _moveAction;
+        private InputAction _swingAction;
 
         [SerializeField]
         private float moveSpeed = 0.02f;
-
-        [SerializeField]
-        private float movementTolerance = 0.01f;
+        //
+        // [SerializeField]
+        // private float movementTolerance = 0.01f;
 
         [SerializeField]
         private Vector2 startPosition = new (0,0);
@@ -22,10 +23,14 @@ namespace Scrips
         [SerializeField]
         private NetworkVariable<Vector2> movement = new();
 
+        [SerializeField]
+        private NetworkVariable<float> rotation = new();
+
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
             _moveAction = _playerInput.actions["Move"];
+            _swingAction = _playerInput.actions["Swing"];
         }
 
         private void OnEnable()
@@ -33,6 +38,7 @@ namespace Scrips
             _playerInput.actions.Enable();
 
             _moveAction.performed += MoveActionOnPerformed;
+            _swingAction.performed += SwingActionOnPerformed;
         }
 
         private void OnDisable()
@@ -40,11 +46,12 @@ namespace Scrips
             _playerInput.actions.Disable();
 
             _moveAction.performed -= MoveActionOnPerformed;
+            _swingAction.performed -= SwingActionOnPerformed;
         }
 
         private void Start()
         {
-            transform.position = startPosition;
+            transform.position = new Vector3(startPosition.x, 0, startPosition.y);
         }
 
         private void Update()
@@ -55,17 +62,20 @@ namespace Scrips
 
         private void UpdateServer()
         {
-            var localTransform = transform;
-            var position = localTransform.position;
+            // var localTransform = transform;
+            // var position = localTransform.position;
+            // var newPosition = new Vector2(
+            //     position.x + movement.Value.x,
+            //     position.z + movement.Value.y);
+            //
+            // if (Math.Abs(position.x - newPosition.x) < movementTolerance && Math.Abs(position.z - newPosition.y) < movementTolerance)
+            //     return;
+            //
+            // localTransform.position = new Vector3(newPosition.x, 0, newPosition.y);
 
-            var newPosition = new Vector2(
-                position.x + movement.Value.x,
-                position.z + movement.Value.y);
-
-            if (Math.Abs(position.x - newPosition.x) < movementTolerance && Math.Abs(position.z - newPosition.y) < movementTolerance)
-                return;
-
-            localTransform.position = new Vector3(newPosition.x, 0, newPosition.y);
+            var body = GetComponent<Rigidbody>();
+            body.AddForce(new Vector3(movement.Value.x * Time.deltaTime, 0, movement.Value.y * Time.deltaTime), ForceMode.VelocityChange);
+            body.AddTorque(new Vector3(0, rotation.Value * Time.deltaTime, 0), ForceMode.VelocityChange);
         }
 
         private void MoveActionOnPerformed(InputAction.CallbackContext context)
@@ -83,5 +93,20 @@ namespace Scrips
         {
             movement.Value = newMovement;
         }
+
+        private void SwingActionOnPerformed(InputAction.CallbackContext context)
+        {
+            if (!(IsClient && IsOwner))
+                return;
+
+            SwingServerRpc(context.ReadValueAsButton());
+        }
+
+        [ServerRpc]
+        private void SwingServerRpc(bool isPressing)
+        {
+            rotation.Value = isPressing ? 50 : 0;
+        }
+
     }
 }
